46087860d1d21c46a34b1f8a9588f2ed9fb6943a
Laura Alves <105663730+lauralves@users.noreply.github.com>
2023-10-23 18:40:52 -0300

    refactor: AtualizarProdutoService

--- src/main/java/com/example/demo/service/AtualizarProdutoService.java
+++ src/main/java/com/example/demo/service/AtualizarProdutoService.java
@@ -6,6 +6,7 @@ import com.example.demo.domain.Produto;
 import com.example.demo.repository.EstoqueRepository;
 import com.example.demo.repository.HistoricoProdutoRepository;
 import com.example.demo.repository.ProdutoRepository;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,14 +18,13 @@ import java.time.LocalDateTime;
 @Service
 @AllArgsConstructor
 public class AtualizarProdutoService {
-    @Autowired
-    private ProdutoRepository produtoRepository;
-    @Autowired
-    private HistoricoProdutoRepository historicoProdutoRepository;
+
+    private final ProdutoRepository produtoRepository;
     @Transactional
     public Produto exec(Long id, Produto resource) {
         var produto = produtoRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Produto não encontrado" + id));
+                .orElseThrow(() -> new EntityNotFoundException("Produto não encontrado" + id));
+        validateProdutoDescontinuado(produto);
 
         var estoque = produto.getEstoque();
         estoque.setDtEntrada(LocalDateTime.now());
@@ -39,21 +39,25 @@ public class AtualizarProdutoService {
 
         Long qtAtual = produto.getQuantidadeDisponivel();
         Long qtEntradaEstoque = resource.getEstoque().getQuantidadeEntrada();
-        //para atualizar a quantidade disponivel de acordo com a quantidade que irá entrar no estoque, acrescida do que já tem
+        //para atualizar a quantidade disponivel de acordo com a quantidade que irá entrar no estoque,
+        // acrescida do que já tem
         if (qtAtual != null) {
             produto.setQuantidadeDisponivel(qtAtual + qtEntradaEstoque);
         }
-        HistoricoProduto historicoProduto = new HistoricoProduto();
-//        var historicoProduto = new HistoricoProduto();
+        var historicoProduto = new HistoricoProduto();
         historicoProduto.setDtEntrada(produto.getEstoque().getDtEntrada());
+        historicoProduto.setDtCriacao(LocalDateTime.now());
         historicoProduto.setNome(resource.getNome());
         historicoProduto.setQuantidadeEntrada(resource.getEstoque().getQuantidadeEntrada());
         historicoProduto.setFornecedor(resource.getFornecedor());
         historicoProduto.setPrecoCompraUnitario(resource.getPrecoCompraUnitario());
         historicoProduto.setPrecoVendaUnitario(resource.getPrecoVendaUnitario());
-        historicoProduto.setDtCriacao(LocalDateTime.now());
         produto.getHistoricoProdutos().add(historicoProduto);
         return produtoRepository.save(produto);
+    }
 
+    private void validateProdutoDescontinuado(Produto resource){
+        Assert.isTrue(resource.getDescontinuado() != true,
+                "O produto foi descontinuado, portanto, não pode ser alterado. ");
     }
 }
