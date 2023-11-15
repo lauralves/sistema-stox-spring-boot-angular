c89bef999d986885e7fe853225195c6c4636e142
Laura Alves <laura-alves1@live.com>
2023-11-14 23:35:03 -0300

    refactor: ajuste no sidebar-menu da aplicação

--- src/main/webapp/app/menu/tab-side-menu/tab-menu-principal.component.ts
+++ src/main/webapp/app/menu/tab-side-menu/tab-menu-principal.component.ts
@@ -1,35 +1,38 @@
-import {Component, ElementRef, EventEmitter, Input, NgModule, OnInit, Output} from '@angular/core';
+import {Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
 import {MenuItem} from "primeng/api";
 import {Router} from "@angular/router";
 import {navigation} from "../../app-navigation";
+import {Sidebar} from "primeng/sidebar";
+import {relative} from "@angular/compiler-cli";
 
 @Component({
   selector: 'app-tab-menu',
   templateUrl: './tab-menu-principal.component.html',
   styleUrls: ['./tab-menu-principal.component.scss']
 })
-export class TabMenuPrincipalComponent implements OnInit{
+export class TabMenuPrincipalComponent implements OnInit {
+  // @ViewChild(Sidebar) sideBar: Sidebar
 
   items: MenuItem[] | undefined;
+  sidebarVisible: boolean = false;
 
-  activeItem: MenuItem | undefined;
-  @Output() sidebarVisible: boolean = false;
-
-  constructor(private router: Router, private elementRef: ElementRef) {
+  constructor(private router: Router, elementRef: ElementRef) {
   }
 
-  emitButton(){
-   return this.sidebarVisible;
-  }
 
   ngOnInit() {
-    this.router.events.subscribe(value => {
-      this.items = navigation
-      this.activeItem = this.items[0];
-    })
+    this.createMenu();
+  }
+
+  redirectToPage(item: MenuItem) {
+    if(item.routerLink){
+      this.router.navigate([item.routerLink]).then(() => {});
+      this.sidebarVisible = false;
     }
+  }
 
-
-
+  private createMenu() {
+    this.items = navigation
+  }
 
 }
