3cb30adf3557c118361a5f116af7256c389d6825
Laura Alves <laura-alves1@live.com>
2023-11-22 16:18:26 -0300

    feat: implementando side-nav-outer-tool-bar.component

---
+++ src/main/webapp/app/menu/side-nav-outer-tool-bar/side-nav-outer-tool-bar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side-nav-outer-tool-bar',
+  templateUrl: './side-nav-outer-tool-bar.component.html',
+  styleUrls: ['./side-nav-outer-tool-bar.component.scss']
+})
+export class SideNavOuterToolBarComponent {
+
+}
