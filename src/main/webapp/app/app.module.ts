c8b60e56af9a7c5cdd96bd9f5464b8f3d6c0f165
Laura Alves <laura-alves1@live.com>
2023-11-25 17:41:16 -0300

    refactor: menu

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -4,27 +4,26 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {PanelMenuModule} from "primeng/panelmenu";
-import {TabMenuModule, TabMenuTemplates} from "primeng/tabmenu";
-import {TabMenuModulePrincipal} from "./menu/tab-side-menu/tab-menu-principal.module";
-import { TopHeaderMenuComponent } from './menu/top-header-menu/top-header-menu.component';
+import {TabMenuModule} from "primeng/tabmenu";
+import {TopHeaderMenuComponent} from './menu/top-header-menu/top-header-menu.component';
 import {PanelModule} from "primeng/panel";
-import { TableModule } from 'primeng/table';
+import {TableModule} from 'primeng/table';
 import {SideNavOuterToolBarComponent} from "./menu/side-nav-outer-tool-bar/side-nav-outer-tool-bar.component";
 import {HttpClientModule} from "@angular/common/http";
+import {TabMenuPrincipalComponent} from "./menu/tab-side-menu/tab-menu-principal.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     TopHeaderMenuComponent,
     SideNavOuterToolBarComponent,
-    SideNavOuterToolBarComponent,
+    TabMenuPrincipalComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     PanelMenuModule,
     TabMenuModule,
-    TabMenuModulePrincipal,
     PanelModule,
     TableModule,
     HttpClientModule
