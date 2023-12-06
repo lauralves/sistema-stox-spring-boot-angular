c33da65f71979cfb431cced99259b72309a3fd01
Laura Alves <laura-alves1@live.com>
2023-12-06 19:48:27 -0300

    refactor: ajuste em fornecedor ts

--- src/main/webapp/app/fornecedor-page/fornecedor.component.ts
+++ src/main/webapp/app/fornecedor-page/fornecedor.component.ts
@@ -1,5 +1,4 @@
 import { Component, Injector, OnInit } from '@angular/core';
-import { ProdutoDialogComponent } from "../produto-page/produto-dialog/produto-dialog.component";
 import { DialogService, DynamicDialogRef } from "primeng/dynamicdialog";
 import { ConfirmEventType, ConfirmationService, MessageService } from "primeng/api";
 import { FornecedorListView } from "../domain/fornecedor-list-view";
@@ -25,15 +24,18 @@ export class FornecedorComponent implements OnInit {
     })
   }
   create() {
-    this.openNovoProdutoDialog();
+    this.openNovoFornecedorDialog();
   }
   delete(id: number) {
-    this.openDeleteProdutoDialog(id);
+    this.openDeleteFornecedorDialog(id);
+  }
+  edit(id: number) {
+
   }
   ngOnInit() {
   }
 
-  openNovoProdutoDialog() {
+  openNovoFornecedorDialog() {
     this.ref = this.dialogService.open(FornecedorDialogComponent, {
       header: 'Cadastrar novo fornecedor',
       width: '30%',
@@ -44,7 +46,7 @@ export class FornecedorComponent implements OnInit {
     });
   }
 
-  openDeleteProdutoDialog(id: number) {
+  openDeleteFornecedorDialog(id: number) {
     this.confirmationService.confirm({
       message: 'Tem certeza que deseja excluir?',
       header: 'Confirmar exclusão',
@@ -54,14 +56,15 @@ export class FornecedorComponent implements OnInit {
             .subscribe(() => {
               this.messageService.add({ severity: 'info', detail: 'Fornecedor excluído com sucesso' });
             })
+            this.reload();
       },
       reject: (type: any) => {
         switch (type) {
           case ConfirmEventType.REJECT:
-            this.messageService.add({ severity: 'error', detail: 'You have rejected' });
+            this.messageService.add({ severity: 'error', detail: 'Você não excluiu' });
             break;
           case ConfirmEventType.CANCEL:
-            this.messageService.add({ severity: 'warn', summary: 'Cancelled', detail: 'You have cancelled' });
+            this.messageService.add({ severity: 'warn', detail: 'Você cancelou' });
             break;
         }
       }
